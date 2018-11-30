b84b4f8ddb6236e20d4fe055edef6ea20f3e88a9
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-11-29 23:30:10 -0500

    created basic framework layout

--- app/src/main/java/com/alphadevelopmentsolutions/frcscout/Activities/MainActivity.java
+++ app/src/main/java/com/alphadevelopmentsolutions/frcscout/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.alphadevelopmentsolutions.frcscout;
+package com.alphadevelopmentsolutions.frcscout.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.alphadevelopmentsolutions.frcscout.R;
 
 public class MainActivity extends AppCompatActivity
 {
