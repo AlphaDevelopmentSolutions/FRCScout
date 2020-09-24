c3b935bf5bf1edbe7638ecc5253dd6a9ad0de259
Griffin Sorrentino <griffinsorrentino@gmail.com>
2020-09-24 19:35:53 -0400

    updated main activity

---
+++ app/src/main/java/com/alphadevelopmentsolutions/frcscout/enums/NavbarState.kt
@@ -0,0 +1,11 @@
+package com.alphadevelopmentsolutions.frcscout.enums
+
+/**
+ * Navbar states for navigation drawer and toolbar
+ */
+enum class NavbarState {
+    LOCKED,
+    LOCKED_WITH_BACK,
+    DRAWER,
+    EDIT
+}
