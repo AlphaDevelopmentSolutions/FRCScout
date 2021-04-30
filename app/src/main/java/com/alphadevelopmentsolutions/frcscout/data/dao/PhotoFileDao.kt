733bf3a0af61169620deb0e1b4b142ce1a5d961a
Griffin Sorrentino <griffinsorrentino@gmail.com>
2021-04-29 23:07:15 -0400

    Updated dependencies

--- app/src/main/java/com/alphadevelopmentsolutions/frcscout/data/dao/PhotoFileDao.kt
+++ app/src/main/java/com/alphadevelopmentsolutions/frcscout/data/dao/PhotoFileDao.kt
@@ -1,15 +1,15 @@
 package com.alphadevelopmentsolutions.frcscout.data.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.alphadevelopmentsolutions.frcscout.data.models.PhotoFile
 import com.alphadevelopmentsolutions.frcscout.interfaces.TableName
-import io.reactivex.Flowable
 
 @Dao
 abstract class PhotoFileDao : MasterDao<PhotoFile>() {
     @Query("SELECT * FROM ${TableName.PHOTO_FILE}")
-    abstract fun getAll(): Flowable<List<PhotoFile>>
+    abstract fun getAll(): LiveData<List<PhotoFile>>
 
     @Query("SELECT * FROM ${TableName.PHOTO_FILE}")
     abstract fun getAllRaw(): List<PhotoFile>
